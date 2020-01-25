c80e92f8a69d3a8f8bc3fbe6fb4ca7ecdceae71e
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-25 23:37:51 +0200

    Add files via upload

---
+++ creating Delegate/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace creating_Delegate
+{
+    class Program
+    {
+        delegate bool IntPredicate(int number);
+        static void Main(string[] args)
+        {
+            IntPredicate funcPredicate = x => x % 2 == 0;
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(funcPredicate(number));
+
+        }
+    }
+}
