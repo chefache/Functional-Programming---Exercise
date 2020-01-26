593f749df2e77f4346f51432bfc517975e7c142f
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-26 23:10:16 +0200

    Add files via upload

---
+++ Add VAT/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prices = Console.ReadLine()
+                 .Split(", ")
+                 .Select(double.Parse);
+
+            var VatPrice = prices.Select(x => x * 1.2);
+            foreach (var price in VatPrice)
+            {
+                Console.WriteLine($"{price:f2}");
+            }
+        }
+    }
+}
