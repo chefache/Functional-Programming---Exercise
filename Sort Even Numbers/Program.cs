13584184a8fe0c65d09a34fc237a004f9de6fa11
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-26 23:11:24 +0200

    Add files via upload

---
+++ Sort Even Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Sort_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .OrderBy(x => x)
+                .Where(x => x % 2 == 0);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
