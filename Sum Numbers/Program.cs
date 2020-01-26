80ef0188a1a492214766e3f0f4f7be8d2da9fb2d
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-26 23:10:47 +0200

    Add files via upload

---
+++ Sum Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(", ")
+                .Select(Parse);
+
+            Console.WriteLine(string.Join(", ", numbers));
+            Console.WriteLine(numbers.Count());
+            Console.WriteLine(numbers.Sum());
+
+        }
+        static int Parse(string str)
+        {
+            int number = 0;
+            foreach (var ch in str)
+            {
+                number *= 10;
+                number += ch - '0';
+            }
+
+            return 0;
+        }
+    }
+}
