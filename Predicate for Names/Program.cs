7e8a5ff02ef438dde09f0b40296609f0c276055b
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-27 23:45:41 +0200

    Add files via upload

---
+++ Predicate for Names/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Predicate_for_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nameLength = int.Parse(Console.ReadLine());
+
+            var names = Console.ReadLine()
+                .Split();
+
+            var resultNames = names.Where(x => x.Length <= nameLength).ToArray();
+            Console.WriteLine(string.Join(" ", resultNames));
+        }
+    }
+}
