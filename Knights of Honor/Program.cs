ccdf43d6fa94fc41de6ae20739ef31034c4f9e10
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-27 23:45:12 +0200

    Add files via upload

---
+++ Knights of Honor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine()
+                .Split();
+
+            Action<string[]> print = items => 
+            Console.WriteLine("Sir " + string.Join(Environment.NewLine + "Sir ", names));
+
+            print(names);
+        }
+    }
+}
