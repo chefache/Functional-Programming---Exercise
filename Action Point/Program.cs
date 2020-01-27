f939593fadbc5b3907d9a5d30616feffbbb1702d
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-27 23:44:02 +0200

    Add files via upload

---
+++ Action Point/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Action_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine()
+              .Split(" ");
+
+            Action<string[]> printToConsole = items => 
+            Console.WriteLine(string.Join(Environment.NewLine, items));
+
+            printToConsole(names);
+        
+        }
+    }
+}
