d7605fe5c98f1d19359f136f756606f64a4d5ea9
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-27 23:45:56 +0200

    Add files via upload

---
+++ Reverse and Exclude/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Reverse_and_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int[]> print = x => Console.WriteLine(string.Join(" ", x)); 
+            var array = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse);
+
+            int number = int.Parse(Console.ReadLine());
+
+            Predicate<int> filter = x => x % number != 0;
+            Func<int, bool> filterFunc = x => filter(x);
+
+            array = array
+                .Where(filterFunc);
+
+            print(array.Reverse().ToArray());
+        }
+    }
+}
