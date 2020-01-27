78013606d0ee32f617f7e9aab3f7623769fa1494
Stefan Koev <stefan.m.koev@gmail.com>
2020-01-27 23:44:37 +0200

    Add files via upload

---
+++ Custom Min Function/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> printSmallest = inputNums =>
+            {
+                int minNum = int.MaxValue;
+
+                foreach (var currentNum in inputNums)
+                {
+                    if (currentNum < minNum)
+                    {
+                        minNum = currentNum;
+                    }
+                }
+                return minNum;
+            };
+
+            var nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int result = printSmallest(nums);
+            Console.WriteLine(result);
+        }
+    }
+}
